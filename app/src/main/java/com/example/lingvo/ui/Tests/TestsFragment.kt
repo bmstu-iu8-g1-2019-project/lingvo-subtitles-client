d376e14ff661f993f747423bb011bd0e7e562ad8
David548219 <david548219@gmail.com>
2019-10-19 18:17:23 +0300

    Updated template to match project requirements

--- app/src/main/java/com/example/lingvo/ui/Tests/TestsFragment.kt
+++ app/src/main/java/com/example/lingvo/ui/Tests/TestsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.lingvo.ui.dashboard
+package com.example.lingvo.ui.Tests
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.lingvo.R
 
-class DashboardFragment : Fragment() {
+class TestsFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
+    private lateinit var testsViewModel: TestsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(this, Observer {
+        testsViewModel =
+            ViewModelProviders.of(this).get(TestsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_tests, container, false)
+        val textView: TextView = root.findViewById(R.id.text_search)
+        testsViewModel.text.observe(this, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/example/lingvo/ui/user/UserFragment.kt
+++ app/src/main/java/com/example/lingvo/ui/user/UserFragment.kt
@@ -1,4 +1,4 @@
-package com.example.lingvo.ui.home
+package com.example.lingvo.ui.user
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.lingvo.R
 
-class HomeFragment : Fragment() {
+class UserFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var userViewModel: UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
+        userViewModel =
+            ViewModelProviders.of(this).get(UserViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_user, container, false)
+        val textView: TextView = root.findViewById(R.id.text_user)
+        userViewModel.text.observe(this, Observer {
             textView.text = it
         })
         return root
